c89e6b40d0d9c516a618add9397718b15613b24b
Max Nadeau <nadeau.max@gmail.com>
2017-01-24 15:44:26 -0500

    This is a test of git stuff on my computer…

--- Elon/src/org/usfirst/frc/team6201/robot/OI.java
+++ Elon/src/org/usfirst/frc/team6201/robot/OI.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.Joystick;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
- * 
+ * Hello this is an edit to the code
  * @author Baxter Ellard
  * 
  */
